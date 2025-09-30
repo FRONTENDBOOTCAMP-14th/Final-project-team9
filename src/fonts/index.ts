28d012f37b0465d719c6f7473857bb590835ac49
dohyeon0709 <dohy0709@naver.com>
2025-09-30 16:31:39 +0900

    init : 프로젝트 초기 구성 - 정적 이미지 추가 - 폰트 추가(여기어때 잘난체, Min-Sans) - custom property 추가

---
+++ src/fonts/index.ts
@@ -0,0 +1,37 @@
+import localFont from 'next/font/local'
+
+// --------------------------------------------------------------------------
+// Local Fonts
+
+// 여기어때 잘난체
+
+export const jalnan = localFont({
+  src: '/Jalnan2TTF.ttf',
+  weight: '700',
+  display: 'swap',
+  style: 'normal',
+})
+
+// Min Sans
+
+export const minSans = localFont({
+  variable: '--min-sans',
+  src: [
+    {
+      path: '/MinSansVF.ttf',
+      weight: '100 900',
+      style: 'normal',
+    },
+  ],
+  display: 'swap',
+})
+
+// --------------------------------------------------------------------------
+// Fonts Module
+
+const fonts = {
+  jalnan,
+  minSans,
+}
+
+export default fonts
