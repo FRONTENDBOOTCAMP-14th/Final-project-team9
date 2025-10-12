91a871787cbf5102a3f932f7713cd19d405844c0
dohyeon0709 <dohy0709@naver.com>
2025-10-12 14:05:55 +0900

    feat : 검색바 컴포넌트 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
-import Dropdown from "@/components/common/input/Dropdown";
+import Dropdown from '@/components/common/input/Dropdown'
+import SearchBar from '@/components/common/search-bar/SearchBar'
 
 export default function Home() {
   return (
     <main>
       <div>9in구직 화이팅</div>
       <Dropdown
-        options={["앱 개발", "웹 개발", "백엔드", "디자인", "기획"]}
+        options={['앱 개발', '웹 개발', '백엔드', '디자인', '기획']}
         placeholder="분야"
       />
+      <SearchBar />
     </main>
-  );
+  )
 }
