b250c24fd974deaaed5042ba097e973767045fef
jungyeon0708 <kimkjy0708@gmail.com>
2025-10-13 16:17:34 +0900

    fix : mainpage find-project 경로수정

--- src/components/mainpage/MainPageButton.tsx
+++ src/components/mainpage/MainPageButton.tsx
@@ -9,7 +9,7 @@ interface ButtonData {
 
 const buttonData: ButtonData[] = [
   {
-    href: "/find-projects",
+    href: "/find-project",
     text: "프로젝트 찾기",
     ariaLabel: "프로젝트 찾기 페이지로 이동",
   },
