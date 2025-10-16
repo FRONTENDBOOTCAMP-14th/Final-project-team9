fa1c2dbdc12222e89230f290aceffbccf30490fd
jungyeon0708 <kimkjy0708@gmail.com>
2025-10-16 12:24:23 +0900

    refactor : 지원하기 버튼 기존 버튼 컴포넌트로 교체

--- src/components/project-detail/index.ts
+++ src/components/project-detail/index.ts
@@ -1,5 +1,4 @@
 export { default as ApplyModal } from "./ApplyModal";
-export { default as ApplyButton } from "./ApplyButton";
 export { default as SuccessToast } from "./SuccessToast";
 export { default as ProjectDetailHeader } from "./ProjectDetailHeader";
 export { default as ProjectDetailContent } from "./ProjectDetailContent";
