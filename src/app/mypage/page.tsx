35af95ee6bb9180d4f0fec4e22425ba578929297
jungyeon0708 <kimkjy0708@gmail.com>
2025-10-27 16:19:39 +0900

    feat : mypage 메타데이터추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,16 @@
+import type { Metadata } from "next";
 import Footer from "@/components/common/footer/Footer";
 import GoTopButton from "@/components/common/GoTopButton";
 import Header from "@/components/common/header/Header";
 import MyPageForm from "@/components/mypage/MyPageForm";
+
+export const metadata: Metadata = {
+  title: "마이페이지 | JOYIN",
+  description: "내 프로필과 프로젝트를 관리하세요!",
+  icons: {
+    icon: "/assets/joyin-fav.ico",
+  },
+};
 
 export default async function MyPage() {
   return (
