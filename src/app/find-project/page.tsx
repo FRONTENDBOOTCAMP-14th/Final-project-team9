bee7fa819e6336dd51b15c3c7876265cda5f2595
dohyeon0709 <dohy0709@naver.com>
2025-10-27 13:45:06 +0900

    feat : 프로젝트 찾기 페이지 메타데이터 추가

--- src/app/find-project/page.tsx
+++ src/app/find-project/page.tsx
@@ -1,7 +1,16 @@
+import type { Metadata } from "next";
 import Footer from "@/components/common/footer/Footer";
 import GoTopButton from "@/components/common/GoTopButton";
 import Header from "@/components/common/header/Header";
 import FindProjectContent from "@/components/find-project/FindProjectContent";
+
+export const metadata: Metadata = {
+  title: "프로젝트 찾기 | JOYIN",
+  description: "프로젝트를 검색하고 딱 맞는 프로젝트을 찾아보세요!",
+  icons: {
+    icon: "/assets/joyin-fav.ico",
+  },
+};
 
 export default function FindProjectPage() {
   return (
