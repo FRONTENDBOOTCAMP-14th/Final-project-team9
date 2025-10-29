519441f38435d51faa415a2bfa134491b4e2ca42
jungyeon0708 <kimkjy0708@gmail.com>
2025-10-29 10:46:09 +0900

    fix : project-card 내부 경력사항 마이페이지와 동기화

--- src/hooks/useProfileForm.ts
+++ src/hooks/useProfileForm.ts
@@ -12,7 +12,7 @@ const MAX_INTRODUCTION_LENGTH = 100;
 
 export function useProfileForm(
   initialUser: Omit<UserProfileCardProps, "projectCounts">,
-  onSave: (updatedUser: Omit<UserProfileCardProps, "projectCounts">) => void,
+  onSave: (updatedUser: Omit<UserProfileCardProps, "projectCounts">) => void
 ) {
   // 기본값을 명시하여 런타임에 일부 필드가 없을 때 발생하는 에러를 방지
   const defaultInitial = {
@@ -117,7 +117,7 @@ export function useProfileForm(
   };
 
   const handleChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>,
+    e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>
   ) => {
     const { name, value } = e.target;
     // 살균처리 적용
